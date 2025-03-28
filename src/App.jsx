fe6e641a9676930d7bc7814539dc49f0b974c685
Ayessa <ayessamaedeguzman78@gmail.com>
2025-03-28 13:09:11 +0800

    created ui for Fastest

--- src/App.jsx
+++ src/App.jsx
@@ -92,10 +92,14 @@ function App() {
   return (
     <>
       <Timer minutes={minutes} seconds={seconds} ms={ms} />
+      <div className="grid-cols-2 grid">
+        <Fastest
+          fastestTime={solveArraySorted.sort((a, b) => a.time - b.time)}
+        />
+        <History solveArray={solveArray} onResetHistory={handleResetHistory} />
+        <Average solveArray={solveArray} />
+      </div>
       <Space onClick={handleSpaceClick} spaceDown={spaceDown} />
-      <Fastest fastestTime={solveArraySorted.sort((a, b) => a.time - b.time)} />
-      <Average solveArray={solveArray} />
-      <History solveArray={solveArray} onResetHistory={handleResetHistory} />
     </>
   );
 }
@@ -112,6 +116,7 @@ function Timer({ minutes, seconds, ms }) {
           )}
           <span className="digits">{String(seconds).padStart(2, "0")}.</span>
           <span className="digits mili-sec">{String(ms).padStart(2, "0")}</span>
+          <span className="ml-2 text-3xl">s</span>
         </div>
       </div>
     </div>
@@ -120,20 +125,39 @@ function Timer({ minutes, seconds, ms }) {
 
 function History({ solveArray, onResetHistory }) {
   return (
-    <div className="mt-20 w-fit rounded-2xl mx-auto p-[3px] bg-gradient-to-b from-orange-500 to-orange-500/10">
+    <div className="row-span-2 mt-20 w-fit rounded-2xl mx-auto p-[3px] bg-gradient-to-b from-orange-500 to-orange-500/10">
       <div className="bg-slate-900/80 mx-auto p-8 rounded-[calc(1rem-3px)] w-110 flex flex-col items-center justify-center gap-5">
-        <p className="underline-offset-3 underline text-center text-3xl font-bold text-orange-500 drop-shadow-[0_5px_5px_rgba(255,105,0,.3)]">
-          History
-        </p>
-        
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-12 text-orange-500/50 stroke-2 mx-auto drop-shadow-[0_5px_5px_rgba(255,105,0,.4)]"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+
+          <p className="underline-offset-3 underline text-center text-3xl font-bold text-orange-500 drop-shadow-[0_5px_5px_rgba(255,105,0,.3)]">
+            History
+          </p>
+        </div>
+
         <div className=" text-white ">
           {solveArray.map((solve) => (
             <div key={solve.id}>
-              <span className="text-orange-500 drop-shadow-[0_5px_5px_rgba(255,105,0,.3)]">Attempt {solve.id}:{" "}</span>
+              <span className="text-orange-500 drop-shadow-[0_5px_5px_rgba(255,105,0,.3)]">
+                Attempt {solve.id}:{" "}
+              </span>
               <span className="drop-shadow-[0_5px_5px_rgba(255,255,255,.3)]">
-              {solve.minutes > 0 ? solve.minutes + " m" : ""}{" "}
-              {String(solve.seconds).padStart(2, "0")} s{" "}
-              {String(solve.ms).padStart(2, "0")} ms
+                {solve.minutes > 0 ? solve.minutes + " m" : ""}{" "}
+                {String(solve.seconds).padStart(2, "0")} s{" "}
+                {String(solve.ms).padStart(2, "0")} ms
               </span>
             </div>
           ))}
@@ -165,18 +189,6 @@ function History({ solveArray, onResetHistory }) {
         )}
       </div>
     </div>
-
-    // <div className="bg-orange-500/10 px-5 py-3 w-45">
-    //   <p className="text-center">History</p>
-    //   <button onClick={onResetHistory}>Delete History</button>
-    //   {solveArray.map((solve) => (
-    //     <div key={solve.id}>
-    //       Attempt {solve.id}: {solve.minutes > 0 ? solve.minutes + " m" : ""}{" "}
-    //       {String(solve.seconds).padStart(2, "0")} s{" "}
-    //       {String(solve.ms).padStart(2, "0")} ms
-    //     </div>
-    //   ))}
-    // </div>
   );
 }
 
@@ -190,20 +202,55 @@ function Space({ onClick, spaceDown }) {
 
 function Fastest({ fastestTime }) {
   return (
-    <div className="bg-green-500/10 px-5 py-3 w-45">
-      <p>
-        Fastest Time:{" "}
-        {fastestTime.length > 0 ? (
-          <span>
-            {fastestTime[0].minutes > 0 ? `${fastestTime[0].minutes} m` : ""}{" "}
-            {String(fastestTime[0].seconds).padStart(2, "0")} s{" "}
-            {String(fastestTime[0].ms).padStart(2, "0")} ms
-          </span>
-        ) : (
-          ""
-        )}
-      </p>
+    <div className="row-span-2 h-fit mt-20 w-fit rounded-2xl mx-auto p-[3px] bg-gradient-to-b from-green-500 to-green-500/10">
+      <div className="bg-slate-900/80 mx-auto py-5 px-5 rounded-[calc(1rem-3px)] w-110 flex items-center justify-center">
+        <div className="flex items-center gap-3 w-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-10 flex-initial text-green-500/50 stroke-2 mx-auto drop-shadow-[0_5px_5px_rgba(0,201,80,.3)]"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+            />
+          </svg>
+
+          <p className="grow text-left text-2xl font-extrabold text-green-500 drop-shadow-[0_5px_5px_rgba(0,201,80,.4)]">
+            Fastest Time:
+          </p>
+            {fastestTime.length > 0 ? (
+              <span className=" grow text-xl font-semibold drop-shadow-[0_5px_5px_rgba(255,255,255,.3)]">
+                {fastestTime[0].minutes > 0
+                  ? `${fastestTime[0].minutes} m`
+                  : ""}{" "}
+                {String(fastestTime[0].seconds).padStart(2, "0")} s{" "}
+                {String(fastestTime[0].ms).padStart(2, "0")} ms
+              </span>
+            ) : (
+              ""
+            )}
+        </div>
+      </div>
     </div>
+    // <div className="bg-green-500/10 px-5 py-3 w-45">
+    //   <p>
+    //     Fastest Time:{" "}
+    //     {fastestTime.length > 0 ? (
+    //       <span>
+    //         {fastestTime[0].minutes > 0 ? `${fastestTime[0].minutes} m` : ""}{" "}
+    //         {String(fastestTime[0].seconds).padStart(2, "0")} s{" "}
+    //         {String(fastestTime[0].ms).padStart(2, "0")} ms
+    //       </span>
+    //     ) : (
+    //       ""
+    //     )}
+    //   </p>
+    // </div>
   );
 }
 
